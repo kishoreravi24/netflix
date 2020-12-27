e7009dfc6fb49261c00134a6b693a7f822e54d04
kishoreravi24 <kishoreravi24399@gmail.com>
2020-12-27 17:44:03 +0530

    Added main page

--- src/container/main.component.js
+++ src/container/main.component.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Credential from '../component/credential/credential.component';
 import Home from '../component/home.component';
+import MainPage from '../component/start.component';
 
 class Main extends React.Component{
     render(){
@@ -8,6 +10,12 @@ class Main extends React.Component{
             <div className="App">
                 <Router>
                     <Route exact path="/">
+                        <MainPage />
+                    </Route>
+                    <Route exact path="/login1">
+                        <Credential/>
+                    </Route>
+                    <Route exact path="/login">
                         <Home/>
                     </Route>
                 </Router>
